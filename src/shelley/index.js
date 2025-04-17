7145aa9587a3a0b405e0ef31375160a210061f5f
xema <maestrequi@esat-alumni.com>
2025-04-17 19:13:03 +0100

    update Shelley Mannor project

--- src/shelley/index.js
+++ src/shelley/index.js
@@ -3,4 +3,12 @@ window.addEventListener("load", (event) =>{
     document.getElementById("steam_url").addEventListener("click", function(){
         window.open("https://store.steampowered.com/app/2999270/Shelley_Manor/","_blank");
     });
+    
+    document.getElementById("steam_link").addEventListener("click", function(){
+        window.open("https://store.steampowered.com/app/2999270/Shelley_Manor/","_blank");
+    });
+    
+    document.getElementById("chaos_system").addEventListener("click", function(){
+        window.open("https://dev.epicgames.com/documentation/en-us/unreal-engine/chaos-destruction-in-unreal-engine","_blank");
+    });
 })
